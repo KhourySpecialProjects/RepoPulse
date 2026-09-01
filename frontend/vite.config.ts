673d39c9b9e9ac615a12590f1e0b6005b4bee0c2
Spiroslogos <spiro737halkias@gmail.com>
2026-09-01 15:40:52 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: true, //adding this to allow use on coolify?
     proxy: {
       '/api': {
         target: 'http://backend:8000',
