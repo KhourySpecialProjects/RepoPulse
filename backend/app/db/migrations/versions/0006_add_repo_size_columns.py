10324c27b1eabfd9f422ce5cbe84d109745664e8
JaylenZeng <jaylenzeng@gmail.com>
2026-09-14 14:56:11 -0400

    rename conflicting migration file

--- backend/app/db/migrations/versions/0006_add_repo_size_columns.py
+++ backend/app/db/migrations/versions/0006_add_repo_size_columns.py
@@ -15,8 +15,8 @@ from __future__ import annotations
 import sqlalchemy as sa
 from alembic import op
 
-revision = "0005"
-down_revision = "0004"
+revision = "0006"
+down_revision = "0005"
 branch_labels = None
 depends_on = None
 
